340016ad0c66466f92f5e8be36eaec2d85805a9d
Tomas Mancera <100365795+TomasMancera@users.noreply.github.com>
2025-04-27 19:42:05 -0500

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import './About.css'; // Opcional para estilos si quieres
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>Acerca de Nosotros</h1>
+      <p>Este proyecto fue realizado por:</p>
+      <ul>
+        
+        <li>Tomás Mancera 98649</li>
+        <li>Alejandro Salazar 97502</li>
+      </ul>
+      <p>Como parte del curso de Desarrollo Web.</p>
+    </div>
+  );
+}
+
+export default About;
